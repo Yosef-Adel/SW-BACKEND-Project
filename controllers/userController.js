78a6a3cae17fb410675af131baa255dc76326205
Mai <maiabdelhameed16@gmail.com>
2023-04-09 16:40:01 +0200

    merge

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,20 +2,6 @@ const User = require('../models/User');
 const uploadImage = require("../utils/uploadImage");
 const fs = require('fs');
 
-// @route   GET api/users/test
-// @desc    Tests users route
-// @access  Public
-exports.test = (req, res) => {
-    res.json({ msg: 'Users Works' });
-}
-
-// @route   POST api/users/register
-// @desc    Register user
-// @access  Public
-exports.register = (req, res) => {
-    return res.json({ msg: 'Register Works' });
-}
-
 
 //a test to check if the user is authorized
 //the user must be logged in to access this route
@@ -103,8 +89,6 @@ exports.changeToAttendee = async(req,res) => {
         return res.status(400).json({message: "Error in changing view"})
     }
 }
-
-
 
 exports.changeImage = async(req, res)=>{
     try{
